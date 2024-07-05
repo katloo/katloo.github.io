f6d5e037ffa81f9a93ade285bc5fa246ec663c20
Kat Liu <katliu@Kats-Laptop.local>
2024-07-05 11:52:45 +0200

    revert to absolute

--- src/components/MobileMenuBar.tsx
+++ src/components/MobileMenuBar.tsx
@@ -4,7 +4,7 @@ import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const MobileMenuBar = () => {
   const [displayMenu, setDisplayMenu] = useState(false);
-  const displayedStyling = 'fixed items-center z-[999] top-0 left-0 w-[100%] h-[100%] ease-in-out duration-500 bg-white'
+  const displayedStyling = 'absolute items-center z-[999] top-0 left-0 w-[100%] h-[100%] ease-in-out duration-500 bg-white'
   const notDisplayedStyling = 'ease-in-out duration-500 fixed left-[-100%]'
 
   const handleDisplayMenu = () => {
@@ -13,7 +13,7 @@ const MobileMenuBar = () => {
 
   return (
     <div className='block p-4 sm:hidden'>
-      <div className='fixed block z-[1000]'>
+      <div className='absolute block z-[1000]'>
         <button onClick={handleDisplayMenu}>
           { displayMenu ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
         </button>
